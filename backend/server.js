cb8c8cf124ef82017e04cffe486f4350cce1f163
mattnix6 <nirina.ran@smartone.ai>
2024-11-26 11:20:03 +0300

    fix: port vercel

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var ws_1 = require("ws");
-var server = new ws_1.WebSocketServer({ port: 8080 });
+var port = process.env.PORT || 8000;
+var server = new ws_1.WebSocketServer({ port: port });
 var rooms = {};
-server.on('connection', function (socket) {
-    socket.on('message', function (message) {
+server.on("connection", function (socket) {
+    socket.on("message", function (message) {
         var data = JSON.parse(message);
-        if (data.type === 'join') {
+        if (data.type === "join") {
             if (!rooms[data.room]) {
                 rooms[data.room] = [];
             }
             rooms[data.room].push(socket);
-            socket.send(JSON.stringify({ type: 'joined', room: data.room }));
+            socket.send(JSON.stringify({ type: "joined", room: data.room }));
         }
-        else if (data.type === 'move') {
+        else if (data.type === "move") {
             var roomSockets = rooms[data.room] || [];
             roomSockets.forEach(function (client) {
                 if (client !== socket && client.readyState === client.OPEN) {
-                    client.send(JSON.stringify({ type: 'update', move: data.move }));
+                    client.send(JSON.stringify({ type: "update", move: data.move }));
                 }
             });
         }
     });
-    socket.on('close', function () {
+    socket.on("close", function () {
         for (var room in rooms) {
             rooms[room] = rooms[room].filter(function (client) { return client !== socket; });
         }
     });
 });
-console.log('WebSocket server running on ws://localhost:8080');
+console.log("WebSocket server running on ws://localhost:8080");
